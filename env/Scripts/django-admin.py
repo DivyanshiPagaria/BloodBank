acb8e739e7ea8fb34703a90e8961e3a8a9288f1c
DivyanshiPagaria <divyanshipagaria10@gmail.com>
2022-12-01 12:00:58 +0530

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\HP\Downloads\bloodbankmanagement-master\bloodbankmanagement\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
